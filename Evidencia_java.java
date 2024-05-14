9e2e0b1f56011e39864d430b26e9cfa07c7993b3
Jorge Ham <108374676+JorgeHam1@users.noreply.github.com>
2024-05-14 00:36:05 -0600

    Add files via upload

---
+++ Evidencia_java.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.evidencia_java;
+
+/**
+ *
+ * @author user
+ */
+public class Evidencia_java {
+
+    public static void main(String[] args) {
+        Interfaz.INTERFAZ_INICIAL objetoformulario = new Interfaz.INTERFAZ_INICIAL();
+        objetoformulario.setVisible(true);
+    }
+}
